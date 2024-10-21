9ae3ac864edeaf058d6ba84f23a4b812d2d44788
unknown <G00393988@atu.ie>
2024-10-21 14:16:15 +0000

    Lab5 Complete No comments

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Data Respresentation & Querying');
+});
+
+app.get('/hello/:name/:sname', (req, res) => {
+    const name = req.params.name+" "+req.params.sname;
+    res.send(`Hello ${name}`);
+});
+
+
+
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(201).json({ whatever:movies });
+    
+});
+
+const path = require('path');
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname+ '/index.html');
+});
+
+app.get('/name', (req, res) => {
+    const firstname = req.query.firstname;
+    const lastname = req.query.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/name', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    res.send(`Hello ${firstname} ${lastname}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
