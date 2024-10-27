a130a2a6ffb3b2364ca2d02499bd229b877a6a46
unknown <G00393988@atu.ie>
2024-10-27 13:59:06 +0000

    Comments Added

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+// Import the express module
 const express = require('express');
+// Create an express application
 const app = express();
+// Define the port number
 const port = 3000;
 
-
+// Error-handling 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+    console.error(err.stack); 
+    // Send a 500 response
+    res.status(500).send('Something went wrong!'); 
 });
 
+// Route for the home page
 app.get('/', (req, res) => {
-    res.send('Welcome to Data Respresentation & Querying');
+    res.send('Welcome to Data Representation & Querying'); // Send a welcome message
 });
 
+// Route with parameters for greeting by name and surname
 app.get('/hello/:name/:sname', (req, res) => {
-    const name = req.params.name+" "+req.params.sname;
-    res.send(`Hello ${name}`);
+    // Concatenate name and surname
+    const name = req.params.name + " " + req.params.sname; 
+    // Send the greeting message
+    res.send(`Hello ${name}`); 
 });
 
-
-
-
+// Route to get a list of movies in JSON format
 app.get('/api/movies', (req, res) => {
     const movies = [
         {
@@ -44,31 +50,39 @@ app.get('/api/movies', (req, res) => {
             "Poster": "https://example.com/poster3.jpg"
         }
     ];
-    res.status(201).json({ whatever:movies });
-    
+    // Send the movie list with a 201 status code
+    res.status(201).json({ whatever: movies }); 
 });
 
+// Import the path module for handling file paths
 const path = require('path');
 
+// Route to serve an HTML file
 app.get('/index', (req, res) => {
-    res.sendFile(__dirname+ '/index.html');
+    // Send the index.html file from the root directory
+    res.sendFile(__dirname + '/index.html'); 
 });
 
+// Route that greets by firstname and lastname using query parameters
 app.get('/name', (req, res) => {
     const firstname = req.query.firstname;
     const lastname = req.query.lastname;
-    res.send(`Hello ${firstname} ${lastname}`);
+    // Send the greeting message
+    res.send(`Hello ${firstname} ${lastname}`); 
 });
 
+// Import body-parser to parse request bodies
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// POST route to handle form submission and greet by firstname and lastname
 app.post('/name', (req, res) => {
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
-    res.send(`Hello ${firstname} ${lastname}`);
+    res.send(`Hello ${firstname} ${lastname}`); 
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`); 
 });
